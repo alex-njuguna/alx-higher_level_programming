c9219fea6f007bdd753f0b49ad00011927c01e6d
= <=>
2023-05-09 23:44:16 +0300

    advanced

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for i in range(26):
     if i % 2 == 0:
         print("{:c}".format(122 - i), end='')
     else:
-        print("{:c}".format(90 - 1), end='')
+        print("{:c}".format(90 - i), end='')
