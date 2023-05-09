e332694bc0de43f4a6030c68c44c478e50610389
= <=>
2023-05-09 17:33:11 +0300

    mandatory

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 mystr = f"Last digit of {number} is"
 if number < 0:
-    number = -number
-last_number = number % 10
+    last_number = ((number * -1) % 10) * -1
+elif number >= 0:
+    last_number = number % 10
 if last_number > 5:
     print(f"{mystr} {last_number} and is greater than 5")
 elif last_number == 0:
