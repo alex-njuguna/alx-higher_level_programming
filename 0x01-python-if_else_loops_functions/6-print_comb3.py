165cf177a386980f80c450cbef20a84a78c8021e
= <=>
2023-05-09 17:03:02 +0300

    mandatory tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for j in range(i + 1, 10):
         if i != j:
             if i < 8 or (i == 8 and j != 9):
-                print(f"{i}{j}", end=', ')
+                print(f"{:d}{:d}".format(i, j), end=', ')
             else:
-                print(f"{i}{j}")
+                print(f"{:d}{:d}".format(i, j))
