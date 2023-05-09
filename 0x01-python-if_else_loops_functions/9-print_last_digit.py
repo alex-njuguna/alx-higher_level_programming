2d9c9b6fb7db5fc0cc71c0ffcec270fff4749260
= <=>
2023-05-09 16:23:33 +0300

    mandatory tasks

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
 
-    if number < 0:
-        return -(abs(number) % 10)
-    return number % 10
+    if number >= 0:
+        last_digit = number % 10
+    else:
+        last_digit = -(abs(number) % 10)
+    print(last digit)
+    return last_digit
