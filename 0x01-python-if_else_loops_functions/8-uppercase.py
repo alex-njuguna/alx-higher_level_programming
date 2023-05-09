cc4a7a593cb3df44939b12b0a3456e3c50a271fc
= <=>
2023-05-09 16:52:12 +0300

    mandatory tasks

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
 
     for char in str:
         if 97 <= ord(char) <= 122:
-            char = chr(ord(char) -32)
+            char = chr(ord(char) - 32)
         print("{:s}".format(char), end='')
     print()
