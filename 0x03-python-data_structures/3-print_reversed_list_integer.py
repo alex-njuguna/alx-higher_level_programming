c72abdc5384f787c04a3d788bd4c414dea03f52f
= <=>
2023-05-16 01:13:56 +0300

    bug fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
 
-    for i in range(len(my_list) - 1, -1, -1):
+    my_list.reverse()
+    for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
