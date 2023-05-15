eac38d3b4538cee3d4d71b794f93299685f1c40b
= <=>
2023-05-16 01:32:04 +0300

    bug fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,9 +9,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a = tuple_a[0] + tuple_b[0]
     if len(tuple_a) > 1 and len(tuple_b) > 1:
         b = tuple_a[1] + tuple_b[1]
-    elif len(tuple_a) > 1:
+    elif len(tuple_a) > 1 and len(tuple_b) < 2:
         b = tuple_a[1]
-    elif len(tuple_b) > 1:
+    elif len(tuple_b) > 1 and len(tuple_a) < 2:
         b = tuple_b[1]
     else:
         b = 0
