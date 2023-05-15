451d78a4ab25a3e30a397d30f0bee14b5bd870a3
= <=>
2023-05-16 01:22:56 +0300

    bug fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
 
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
-        print()
+    for row in matrix:
+        print(' '.join("{:d}".format(element) for element in row))
