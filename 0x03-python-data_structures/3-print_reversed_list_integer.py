909260a9007b6bd4132f4ce2faf8d639d2a1ecb3
= <=>
2023-05-16 01:10:00 +0300

    bug fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
 
     for i in range(len(my_list) - 1, -1, -1):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
 
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print(matrix[i][j], end=" ")
+            print("{:d}".format(matrix[i][j]), end=" ")
         print()
