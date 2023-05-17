5ed92a0eed7e5bf1c6917f2ee185bd8ae4cce672
= <=>
2023-05-17 14:54:35 +0300

    mandatory tasks

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    mydirs = dir(hidden_4)
-    for mydir in mydirs:
-        if mydir[:2] != "__":
-            print("{:s}".format(mydir))
+    filename = "hidden_4.pyc"
+    with open(filename, "rb") as file:
+        file.seek(8)
+
+        names = []
+        while True:
+            name_length = int.from_bytes(file.read(2), byteorder='little')
+            if name_length == 0:
+                break
+            name = file.read(name_length).decode('utf-8')
+            names.append(name)
+
+    sorted_names = sorted(names)
+    for name in sorted_names:
+        if not name.startswith("__"):
+            print(name)
