d13dc3d2b06f32bd152bea643870589a9781d593
= <=>
2023-05-17 14:34:37 +0300

    mandatory tasks

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,4 @@ from add_0 import add
 a = 1
 b = 2
 result = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result)
+print("{:d} + {:d} = {:d}".format(a, b, result))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 add = 0
-for s in range(len(argv) - 1):
+for i in range(len(argv) - 1):
     add += int(argv[i + 1)
+
 print("{:d}".format(add))
