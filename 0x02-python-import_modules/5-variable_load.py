7402fd75750da8a4f88dc81229d530e5a08e63f1
= <=>
2023-05-17 14:44:03 +0300

    mandatory tasks

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-from variable_load_5 import a
-if __init__ == "__main__":
+if __name__ == "__main__":
+    """some comment here"""
+    from variable_load_5 import a
+
     print(a)
