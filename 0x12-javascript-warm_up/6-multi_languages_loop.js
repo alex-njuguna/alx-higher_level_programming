1f8991081c5119ee5f0259072ad2cb7469f57789
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-14 15:10:35 +0300

    last batch of tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-for (let line of lines) {
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line of lines) {
   console.log(line);
 }
