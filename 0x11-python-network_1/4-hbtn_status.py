9efda5781459dfbe3518e95f9023707a78985f11
alex-njuguna <njugunakinuthia013@gmail.com>
2023-09-02 01:01:50 +0300

    requests 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    url = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(url.text)))
+    print("\t- content: {}".format(url.text))
